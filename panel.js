430b8a83ffd924fb499466cf35f233cd3a8fb0c0
StevenJoeZhang <1119186082@qq.com>
2018-05-09 15:45:01 +0800

    new feature

--- panel.js
+++ panel.js
@@ -9,7 +9,7 @@ const request = require("request");
 const progress = require("progress-stream");
 const async = require("async");
 
-var referer, playUrl, downloadPath, count, links, cid, isDownloading = false;
+var referer, playUrl, count, links, cid, isDownloading = false;
 
 function getVideoUrl() {
 	var videoUrl = $("#videoUrl").val();// || "https://www.bilibili.com/bangumi/play/ep90832";
@@ -92,7 +92,6 @@ function parseData(data) {
 function download(data) {
 	if (isDownloading) return;
 	isDownloading = true;
-	downloadPath = $("#downloadPath").val() || "";
 	var functionArray = new Array();
 	$(".download").show().html("");
 	for (var i = 0; i < count; i++) {
@@ -111,6 +110,7 @@ function download(data) {
 }
 
 function downloadLink(i) {
+	var downloadPath = $("#downloadPath").val() || "";
 	var file = path.join(downloadPath, cid + "-" + i + ".flv");
 	var options = {
 		url: links[i],
@@ -144,15 +144,7 @@ function downloadLink(i) {
 
 function xml() {
 	var url = "https://comment.bilibili.com/" + cid + ".xml";
-	var saveas = document.createElement("a");
-	saveas.href = url;
-	saveas.style.display = "none";
-	document.body.appendChild(saveas);
-	saveas.download = cid + ".xml";
-	saveas.click();
-	setTimeout(function() {
-		saveas.parentNode.removeChild(saveas);
-	}, 1000);
+	blobDownload(url, cid + ".xml");
 }
 
 function ass() {
@@ -171,12 +163,12 @@ function ass() {
 
 function gotFile(name, content) {
 	var danmaku = parseFile(content);
-	console.log(danmaku);
+	//console.log(danmaku);
 	var ass = generateASS(setPosition(danmaku), {
 		"title": document.title,
 		"ori": name,
 	});
-	blobDownload(ass, name.replace(/\.[^.]*$/, "") + ".ass"); //"\ufeff" + 
+	assDownload(ass, name.replace(/\.[^.]*$/, "") + ".ass"); //"\ufeff" + 
 }
 
 function parseFile(content) {
@@ -184,11 +176,18 @@ function parseFile(content) {
     return parseXML(content);
 }
 
-function blobDownload(data, filename) {
+function assDownload(data, filename) {
 	var blob = new Blob([data], {
 		type: "application/octet-stream"
 	});
 	var url = window.URL.createObjectURL(blob);
+	blobDownload(url, filename);
+	document.addEventListener("unload", function() {
+		window.URL.revokeObjectURL(url);
+	});
+}
+
+function blobDownload(url, filename) {
 	var saveas = document.createElement("a");
 	saveas.href = url;
 	saveas.style.display = "none";
@@ -198,7 +197,4 @@ function blobDownload(data, filename) {
 	setTimeout(function() {
 		saveas.parentNode.removeChild(saveas);
 	}, 1000);
-	document.addEventListener("unload", function() {
-		window.URL.revokeObjectURL(url);
-	});
 }
