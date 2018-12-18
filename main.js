ea917540d633cc252959e01a35b026d4a472145b
StevenJoeZhang <1119186082@qq.com>
2018-12-18 20:39:44 +0800

    you-get

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const electron = require("electron");
 // Module to control application life.
 const app = electron.app;
-const ipc = electron.ipcMain;
-const dialog = electron.dialog;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
@@ -11,12 +9,11 @@ const url = require("url");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-var length;
+let panelWindow;
 
 function createPanel() {
 
-	mainWindow = new BrowserWindow({
+	panelWindow = new BrowserWindow({
 		width: 600,
 		minWidth: 600,
 		height: electron.screen.getPrimaryDisplay().workAreaSize.height,
@@ -26,36 +23,26 @@ function createPanel() {
 		title: "Mimi Downloader"
 	});
 
-	mainWindow.loadURL(url.format({
+	panelWindow.loadURL(url.format({
 		pathname: path.join(__dirname, "panel.html"),
 		protocol: "file:",
 		slashes: true
 	}));
 
-	//mainWindow.setPosition(0, 0, true);
+	//panelWindow.setPosition(0, 0, true);
 
 	// Open the DevTools.
 	// mainWindow.webContents.openDevTools()
 
-	mainWindow.on("close", function(e) {
-		//e.preventDefault();
-		const options = {
-			type: "warning",
-			title: "[Warning]",
-			message: "资源正在下载，您确定要强制退出吗？",
-			buttons: ["是", "否"]
-		}
-		if (length && dialog.showMessageBox(options)) e.preventDefault();
-	});
-
 	// Emitted when the window is closed.
-	mainWindow.on("closed", function(e) {
+	panelWindow.on("closed", function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null;
+		panelWindow = null;
 	});
 }
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -67,24 +54,17 @@ app.on("window-all-closed", function() {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== "darwin") {
-		//app.quit();
+		app.quit();
 	}
-});
+})
 
 app.on("activate", function() {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
+	if (panelWindow === null) {
 		createPanel();
 	}
-	else mainWindow.restore();
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-ipc.on("length", function(event, message) {
-	length = message;
-});
-
-process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = true;
--- panel.js
+++ panel.js
@@ -1,34 +1,23 @@
 const path = require("path");
+const url = require("url");
 const fs = require("fs");
 const request = require("request");
 const progress = require("progress-stream");
 const async = require("async");
-const mime = require("mime");
 const electron = require("electron");
 const { dialog, shell } = electron.remote;
-const ipcRender = electron.ipcRenderer;
 
-var videoUrl, playUrl, aid, p = 1, cid, count, links, downloadArray = [], downloadIndex = 0, manual = false;
+var videoUrl, playUrl, count, links, cid, downloadArray = new Array(), downloadIndex = 0;
 var debug = !true;
-
-function showError(text) {
-	dialog.showMessageBox({type:"error", title: "[Error]", message: text});
-}
-
-function showWarning(text) {
-	dialog.showMessageBox({type:"warning", title: "[Warning]", message: text});
-}
 
 function getVideoUrl() {
 	var videoUrl = $("#videoUrl").val();
-	//if (debug) videoUrl = "https://www.bilibili.com/bangumi/play/ep90832";
-	if (debug) videoUrl = "https://www.bilibili.com/video/av23498892";
+	if (debug) videoUrl = "https://www.bilibili.com/bangumi/play/ep90832";
 	if (videoUrl.indexOf("https://") != 0) {
-		if (videoUrl.indexOf("av") != -1) videoUrl = "https://www.bilibili.com/video/av" + videoUrl.split("av")[1];
-		else if (videoUrl.indexOf("ep") != -1) videoUrl = "https://www.bilibili.com/bangumi/play/ep" + videoUrl.split("ep")[1];
-		else if (videoUrl.indexOf("ss") != -1) videoUrl = "https://www.bilibili.com/bangumi/play/ss" + videoUrl.split("ss")[1];
+		if (videoUrl.indexOf("http://") == 0) videoUrl = videoUrl.replace("http://", "https://");
+		else if (videoUrl.indexOf("bilibili") != -1) videoUrl = "https://" + videoUrl;
 		else {
-			showError("无效的视频链接！");
+			alert("[Error]无效的视频链接！");
 			$("#videoUrl").parent().addClass("has-error has-feedback");
 			return null;
 		}
@@ -37,39 +26,12 @@ function getVideoUrl() {
 	return videoUrl;
 }
 
-function getPlayUrl() {
-	var playUrl = $("#playUrl").val();
-	if (debug) playUrl = "https://bangumi.bilibili.com/player/web_api/v2/playurl?cid=11090110&appkey=84956560bc028eb7&otype=json&type=&quality=80&module=bangumi&season_type=1&qn=80&sign=d6d73e8fbbc2adacaf047c48714e8e69";
-	if (playUrl.indexOf("http://") == 0) playUrl = playUrl.replace("http://", "https://");
-	if (playUrl.indexOf("bilibili") != -1 || !playUrl.split("?cid=")[1]) {
-		showError("无效的PlayUrl！");
-		$("#playUrl").parent().addClass("has-error has-feedback");
-		return null;
-	}
-	$("#playUrl").parent().removeClass("has-error has-feedback");
-	return playUrl;
-}
-
-function backupUrl() {
-	showError("获取PlayUrl或下载链接出错，请手动输入PlayUrl！否则由于B站限制，只能下载低清晰度视频！");
-	$("#backup-url, #error").show();
-	$("#playUrl").parent().addClass("has-error has-feedback");
-	//$("#success").hide();
-	$("#playUrl").val("");
-	manual = true;
-}
-
 function getAid() {
-	if (manual) {
-		if (videoUrl != getVideoUrl()) manual = false; //用户在请求playUrl时改变了videoUrl
-		else playUrl = getPlayUrl();
-	}
 	videoUrl = getVideoUrl();
-	if (!videoUrl || (manual && !playUrl)) return;
+	if (!videoUrl) return; // || !playUrl
 
-	if (videoUrl.split("av")[1]) {
-		aid = videoUrl.split("av")[1].split("/")[0];
-		p = videoUrl.split("av")[1].split("?p=")[1] || 1;
+	if (videoUrl.split("/av")[1]) {
+		aid = videoUrl.split("/av")[1].split("/")[0];
 		getInfo();
 	}
 	else {
@@ -77,7 +39,7 @@ function getAid() {
 			type: "get",
 			dataType: "text",
 			error: function(xhr, status, error) {
-				showError("获取视频aid出错！");
+				alert("[Error]获取视频aid出错！");
 			},
 			success: function(data, status, xhr) {
 				aid = data.split("//www.bilibili.com/video/av")[1].split("/")[0];
@@ -92,7 +54,7 @@ function getInfo() {
 		type: "get",
 		dataType: "text",
 		error: function(xhr, status, error) {
-			showError("获取视频信息出错！");
+			alert("[Error]获取视频信息出错！");
 		},
 		success: function(data, status, xhr) {
 			//console.log(data);
@@ -100,9 +62,10 @@ function getInfo() {
 			$("tbody").eq(1).html("");
 			for (var i in data) {
 				if (i == "cid") {
-					//cid = data[i];
+					cid = data[i];
+					playUrl = generatePlayUrl();
 				}
-				if (mime.getType(data[i]) && mime.getType(data[i]).indexOf("image") != -1) { //解析图片地址
+				if (data[i] && data[i].toString().indexOf("hdslb.com") != -1) { //解析图片地址
 					data[i] = '<a href="' + data[i] + '" download=""><img src="' + data[i] + '"></a>';
 				}
 				$("tbody").eq(1).append("<tr>\
@@ -110,90 +73,66 @@ function getInfo() {
 				<td>" + data[i] + "</td>\
 				</tr>");
 			}
-			$.ajax("https://www.bilibili.com/widget/getPageList?aid=" + aid, {
-				type: "get",
-				dataType: "text",
-				error: function(xhr, status, error) {
-					showError("获取视频信息出错！");
-				},
-				success: function(data, status, xhr) {
-					data = JSON.parse(data);
-					cid = data[p - 1].cid;
-					var params = `appkey=84956560bc028eb7&cid=${cid}&otype=json&qn=112&quality=112&type=`,
-						sign = hex_md5(params + "94aba54af9065f71de72f5508f1cd42e");
-					playUrl = `http://interface.bilibili.com/v2/playurl?${params}&sign=${sign}`;
-					if (manual) {
-						playUrl = getPlayUrl();
-						if (cid != playUrl.split("?cid=")[1].split("&")[0]) {
-							//return; //视频地址和PlayUrl不匹配时结束
-							showWarning("视频地址和PlayUrl不匹配，可能造成问题！");
-							cid = playUrl.split("?cid=")[1].split("&")[0];
-						}
-						manual = false;
-					}
-
-					if (!cid) {
-						showError("获取视频cid出错！");
-						return;
-					}
-					getData(playUrl);
-					getDanmaku(); //获取cid后，获取下载链接和弹幕信息
-					$("#nav").show();
-					if ($(".info").eq(1).is(":hidden")) {
-						changeMenu(0);
-						//$(".info").eq(0).fadeIn();
-					}
-				}
-			});
+			getData(playUrl);
 		}
 	});
 }
 
-function getData(url, isBangumi) {
+function generatePlayUrl() {
+	var type = 0, params, sign;
+	if (videoUrl.indexOf("bangumi") != -1 && videoUrl.indexOf("bangumi") != -1) type = 2;
+	else if (videoUrl.indexOf("bangumi") != -1) type = 1;
+	if (type) {
+		if (type == 2) params = "cid=" + cid + "&module=movie&player=1&quality=112&ts=1";
+		else params = "cid=" + cid + "&module=bangumi&player=1&quality=112&ts=1";
+		sign = hex_md5(params + "9b288147e5474dd2aa67085f716c560d");
+		return "http://bangumi.bilibili.com/player/web_api/playurl?" + params + "&sign=" + sign;
+	}
+	else {
+		params = "appkey=84956560bc028eb7&cid=" + cid + "&qn=112&quality=112&type=";
+		sign = hex_md5(params + "94aba54af9065f71de72f5508f1cd42e");
+		return "http://interface.bilibili.com/v2/playurl?" + params + "&sign=" + sign;
+	}
+	//return "http://interface.bilibili.com/v2/playurl?appkey=84956560bc028eb7&otype=json&platform=bilihelper&type=flv&quality=80&qn=80&cid=" + cid;
+}
+
+function getData(url) {
 	$.ajax(url, {
 		type: "get",
-		dataType: "text",
+		dataType: "xml",
 		error: function(xhr, status, error) {
-			backupUrl();
+			alert("[Error]获取PlayUrl或下载链接出错！");
 		},
 		success: function(data, status, xhr) {
 			//console.log(url, data);
-			var data = isBangumi ? $(data) : JSON.parse(data),
-				target = isBangumi ? data.find("durl") : data.durl;
-			if (target) {
-				var quality = isBangumi ? $(data).find("quality").text() : data.quality,
-					qualityArray = {
-					112: "高清 1080P+",
-					80: "高清 1080P",
-					74: "高清 720P60",
-					64: "高清 720P",
-					48: "高清 720P",
-					32: "清晰 480P",
-					16: "流畅 360P",
-					15: "流畅 360P"
-				} //需要修改，不是一一对应
-				$("#quality").html(qualityArray[quality] || "未知");
-				parseData(target, isBangumi);
-			}
-			else {
-				backupUrl();
-				if (isBangumi) return;
-				var params = `cid=${cid}&module=movie&player=1&quality=112&ts=1`;
-				sign = hex_md5(params + "9b288147e5474dd2aa67085f716c560d");
-				getData(`http://bangumi.bilibili.com/player/web_api/playurl?${params}&sign=${sign}`, true);
-			}
+			parseData($(data));
 		}
 	});
 }
 
-function parseData(target, isBangumi) {
-	if (!isBangumi) $("#backup-url, #error").hide();
-	$("#success").show();
+function parseData(data) {
+	var target = data.find("durl");
+	if (!target || !target.length) {
+		alert("[Error]获取PlayUrl或下载链接出错！");
+		return;
+	}
 	$("#cid").html(cid);
-	$("tbody").eq(0).html("");
 	count = target.length;
-	links = [];
-	if (isBangumi) target.each(function(i, o) {
+	var qualityArray = {
+		112: "高清 1080P+",
+		80: "高清 1080P",
+		74: "高清 720P60",
+		64: "高清 720P",
+		48: "高清 720P",
+		32: "清晰 480P",
+		16: "流畅 360P",
+		15: "流畅 360P"
+	} //需要修改，不是一一对应
+	var quality = qualityArray[data.find("quality").text()] || "未知";
+	$("#quality").html(quality);
+	$("tbody").eq(0).html("");
+	links = new Array();
+	target.each(function(i, o) {
 		var part = $(o);
 		links.push(part.find("url").text());
 		$("tbody").eq(0).append("<tr>\
@@ -209,22 +148,8 @@ function parseData(target, isBangumi) {
 			</td>\
 		</tr>");
 	});
-	else for (var i in target) {
-		var part = target[i];
-		links.push(part.url);
-		$("tbody").eq(0).append("<tr>\
-			<td>" + part.order + "</td>\
-			<td>" + part.length / 1e3 + "</td>\
-			<td>" + part.size / 1e6 + "</td>\
-			<td>\
-				<div class=\"checkbox\">\
-					<label>\
-						<input type=\"checkbox\" checked=\"true\">\
-					</label>\
-				</div>\
-			</td>\
-		</tr>");
-	}
+	$("#nav").show();
+	if ($(".info").eq(1).is(":hidden")) $(".info").eq(0).fadeIn();
 }
 
 function openDialog() {
@@ -235,7 +160,7 @@ function openDialog() {
 			"openDirectory", //打开路径
 		],
 		filters: [
-			//{ name: "zby", extensions: ["json"] },
+			//{ name: 'zby', extensions: ['json'] },
 		]
 	}, function(res) {
 		if (res[0]) $("#downloadPath").val(res[0]);
@@ -243,15 +168,12 @@ function openDialog() {
 }
 
 function download(data) {
-	var functionArray = [];
-	var flag = true;
+	var functionArray = new Array();
 	//$("#download").html("");
 	for (var i = 0; i < count; i++) {
 		if ($('input[type="checkbox"]').eq(i).prop("checked")) {
 			if (downloadArray.indexOf(links[i]) != -1) continue;
-			$("#download").append("<span>" + cid + "-" + i + '</span>\
-			&nbsp;&nbsp;&nbsp;\
-			<span class="addon"></span>\
+			$("#download").append('<span>' + cid + "-" + i + '</span>\
 			&nbsp;&nbsp;&nbsp;\
 			<span class="speed"></span>\
 			&nbsp;&nbsp;&nbsp;\
@@ -260,20 +182,17 @@ function download(data) {
 				<div class="progress-bar progress-bar-info" role="progressbar" style="width: 0%;">\
 					<span class="progress-value">0%</span>\
 				</div>\
-			</div>');
+		</div>');
 			let _i = i;
 			let _j = downloadIndex; //必须使用let或const
 			downloadIndex++;
 			downloadArray.push(links[i]);
-			ipcRender.send("length", downloadArray.length);
 			functionArray.push(function(callback) {
 				downloadLink(_i, _j);
 				//callback(null, j + " Done");
 			});
-			flag = false;
 		} //由于js执行机制，此处不能直接传值
 	}
-	if (flag) showWarning("没有新的视频被下载！");
 	async.parallel(functionArray, function(err, results) {
 		if (err) console.log(err);
 	});
@@ -284,16 +203,11 @@ function openPath() {
 }
 
 function downloadLink(i, j) {
-	var downloadPath = $("#downloadPath").val() || "",
-		filename = (count > 10 && i <= 9) ? `${cid}-0${i}.flv` : `${cid}-${i}.flv`,
-		file = path.join(downloadPath, filename);
-	fs.exists(file, function(exist) {
-		if (exist) resumeDownload(i, j, file)
-		else newDownload(i, j, file);
-	});
-}
-
-function newDownload(i, j, file) {
+	var downloadPath = $("#downloadPath").val() || "";
+	var filename;
+	if (count > 10 && i <= 9) filename = cid + "-0" + i + ".flv"
+	else filename = cid + "-" + i + ".flv";
+	var file = path.join(downloadPath, filename);
 	var options = {
 		url: links[i],
 		encoding: null, //当请求的是二进制文件时，一定要设置
@@ -305,28 +219,6 @@ function newDownload(i, j, file) {
 	}
 	//console.log(cid, file, options.url);
 	var downloads = fs.createWriteStream(file);
-	generalDownload(i, j, options, downloads);
-}
-
-function resumeDownload(i, j, file) {
-	fs.stat(file, function(error, state) {
-		var options = {
-			url: links[i],
-			encoding: null, //当请求的是二进制文件时，一定要设置
-			headers: {
-				"Range": `bytes=${state.size}-`, //断点续传
-				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15",
-				"Referer": videoUrl
-			}
-		}
-		$(".addon").eq(j).html(`从${Math.round(state.size / 1e6)}MB处恢复的下载`);
-		//console.log(cid, file, options.url);
-		var downloads = fs.createWriteStream(file, {"flags": "a"});
-		generalDownload(i, j, options, downloads);
-	});
-}
-
-function generalDownload(i, j, options, downloads) {
 	request.get(options).on("response", function(response) {
 		//https://blog.csdn.net/zhu_06/article/details/79772229
 		var proStream = progress({
@@ -335,19 +227,74 @@ function generalDownload(i, j, options, downloads) {
 		});
 		proStream.on("progress", function(progress) {
 			//console.log(progress);
-			$(".speed").eq(j).html(Math.round(progress.speed / 1e3) + "kb/s");
-			$(".eta").eq(j).html(`eta:${progress.eta}s`);
+			$(".speed").eq(j).html(Math.round(progress.speed / 1000) + "kb/s");
+			$(".eta").eq(j).html("eta:" + progress.eta + "s");
 			var percentage = progress.percentage; //显示进度条
 			$(".progress-value").eq(j).html(Math.round(percentage) + "%");
 			$(".progress-bar").eq(j).css("width", percentage + "%");
 			if (percentage == 100) {
 				$(".progress-bar").eq(j).removeClass("progress-bar-info").addClass("progress-bar-success").parent().removeClass("active");
 				downloadArray.splice(downloadArray.indexOf(links[i]), 1);
-				ipcRender.send("length", downloadArray.length);
 			}
 		});
 		request.get(options).pipe(proStream).pipe(downloads).on("error", function(e) {
-			console.error(e);
+  			console.error(e);
 		}); //先pipe到proStream再pipe到文件的写入流中
 	});
+}
+
+function xml() {
+	var url = "https://comment.bilibili.com/" + cid + ".xml";
+	blobDownload(url, cid + ".xml");
+}
+
+function ass() {
+	//使用ajax是因为bilibili采用了content-encoding:deflate压缩，若使用https.get需要zlib库解压，较为复杂
+	$.ajax("https://comment.bilibili.com/" + cid + ".xml", {
+		type: "get",
+		dataType: "text", //避免ajax解析为xml造成问题
+		error: function(xhr, status, error) {
+			alert("[Error]弹幕下载失败！");
+		},
+		success: function(data, status, xhr) {
+			gotFile(cid, data);
+		}
+	});
+}
+
+function gotFile(name, content) {
+	var danmaku = parseFile(content);
+	var ass = generateASS(setPosition(danmaku), {
+		"title": document.title,
+		"ori": name,
+	});
+	assDownload(ass, name + ".ass"); //"\ufeff" + 
+}
+
+function parseFile(content) {
+	content = content.replace(/[\u0000-\u0008\u000b\u000c\u000e-\u001f]/g, "");
+	return parseXML(content);
+}
+
+function assDownload(data, filename) {
+	var blob = new Blob([data], {
+		type: "application/octet-stream"
+	});
+	var url = window.URL.createObjectURL(blob);
+	blobDownload(url, filename);
+	document.addEventListener("unload", function() {
+		window.URL.revokeObjectURL(url);
+	});
+}
+
+function blobDownload(url, filename) {
+	var saveas = document.createElement("a");
+	saveas.href = url;
+	saveas.style.display = "none";
+	document.body.appendChild(saveas);
+	saveas.download = filename;
+	saveas.click();
+	setTimeout(function() {
+		saveas.parentNode.removeChild(saveas);
+	}, 1000);
 }
