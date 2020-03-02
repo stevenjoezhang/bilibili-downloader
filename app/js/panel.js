e00a0c9c2eab050af03d69bd75be93c9ba32095d
Mimi <1119186082@qq.com>
2020-03-02 12:50:15 +0800

    Fix incorrect bangumi cid

--- app/js/panel.js
+++ app/js/panel.js
@@ -8,55 +8,64 @@ const electron = require("electron");
 const { ipcRenderer } = electron;
 const { app, shell } = electron.remote;
 
-var videoUrl, videoName, cid, links, downloadArray = [];
+var video = {};
+var links, downloadArray = [];
 
-function showError(text) {
-	dialog.showMessageBox({type: "error", title: "[Error]", message: text});
+function showError(message) {
+	dialog.showMessageBox({type: "error", title: "[Error]", message});
 }
 
-function showWarning(text) {
-	dialog.showMessageBox({type: "warning", title: "[Warning]", message: text});
+function showWarning(message) {
+	dialog.showMessageBox({type: "warning", title: "[Warning]", message});
 }
 
 function getVideoUrl() {
-	var videoUrl = $("#videoUrl").val();
-	if (videoUrl.indexOf("https://") !== 0) {
-		if (videoUrl.includes("av")) videoUrl = "https://www.bilibili.com/video/av" + videoUrl.split("av")[1];
-		else if (videoUrl.includes("ep")) videoUrl = "https://www.bilibili.com/bangumi/play/ep" + videoUrl.split("ep")[1];
-		else if (videoUrl.includes("ss")) videoUrl = "https://www.bilibili.com/bangumi/play/ss" + videoUrl.split("ss")[1];
-		else {
-			showError("无效的视频链接！");
-			$("#videoUrl").parent().addClass("has-error has-feedback");
-			return null;
-		}
+	let videoUrl = $("#videoUrl").val(), type;
+	if (videoUrl.includes("av")) {
+		type = "av";
+		videoUrl = "https://www.bilibili.com/video/av" + videoUrl.split("av")[1];
+	}
+	else if (videoUrl.includes("ep")) {
+		type = "ep";
+		videoUrl = "https://www.bilibili.com/bangumi/play/ep" + videoUrl.split("ep")[1];
+	}
+	else if (videoUrl.includes("ss")) {
+		type = "ss";
+		videoUrl = "https://www.bilibili.com/bangumi/play/ss" + videoUrl.split("ss")[1];
+	}
+	else {
+		showError("无效的视频链接！");
+		$("#videoUrl").parent().addClass("has-error has-feedback");
+		return null;
 	}
 	$("#videoUrl").parent().removeClass("has-error has-feedback");
-	return videoUrl;
+	return { videoUrl, type };
 }
 
 function getAid() {
-	videoUrl = getVideoUrl();
+	let { videoUrl, type } = getVideoUrl();
 	if (!videoUrl) return;
-	let id = videoUrl.split("av")[1];
-	if (id) {
-		let aid = id.split("/")[0].split("?")[0],
+	video.url = videoUrl;
+	if (type === "av") {
+		let id = video.url.split("av")[1],
+			aid = id.split("/")[0].split("?")[0],
 			pid = id.split("p=")[1] || 1;
-		getInfo(aid, pid);
+		getInfo({ aid, pid });
 	} else {
-		fetch(videoUrl)
+		fetch(video.url)
 			.then(response => response.text())
 			.then(result => {
 				let data = result.match(/__INITIAL_STATE__=(.*?);\(function\(\)/)[1];
+				console.log("INITIAL STATE", data);
 				data = JSON.parse(data);
-				let epId = data.epList[0].id;
-				let { aid } = data.epList[0]; // Not always correct
-				getInfo(aid, 1);
+				let { aid, cid } = type === "ss" ? data.epList[0] : data.epInfo;
+				getInfo({ aid, cid });
 			})
 			.catch(error => showError("获取视频 aid 出错！"));
 	}
 }
 
-function getInfo(aid, pid) {
+function getInfo({ aid, pid, cid }) {
 	fetch("https://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=" + aid)
 		.then(response => response.json())
 		.then(data => {
@@ -71,25 +80,32 @@ function getInfo(aid, pid) {
 				<td style="word-break: break-all;">${data[i]}</td>
 				</tr>`);
 			}
-			//cid = data.cid;
-			videoName = `${data.cid}-${data.title}`;
-			$("#videoName").val(videoName);
+			//video.cid = data.cid;
+			video.name = `${data.cid}-${data.title}`;
+			$("#videoName").val(video.name);
 		})
 		.catch(error => showError("获取视频信息出错！"));
 	fetch("https://www.bilibili.com/widget/getPageList?aid=" + aid)
 		.then(response => response.json())
 		.then(result => {
 			console.log("PAGE LIST", result);
-			cid = result[pid - 1].cid;
-			let params = `appkey=iVGUTjsxvpLeuDCf&cid=${cid}&otype=json&qn=112&quality=112&type=`,
-				sign = crypto.createHash("md5").update(params + "aHRmhWMLkdeMuILqORnYZocwMBpMEOdt").digest("hex"),
-				playUrl = `http://interface.bilibili.com/v2/playurl?${params}&sign=${sign}`;
-
-			if (!cid) {
-				showError("获取视频 cid 出错！");
-				return;
+			if (cid) {
+				video.cid = cid;
+				let params = `cid=${video.cid}&module=movie&player=1&quality=112&ts=1`,
+					sign = crypto.createHash("md5").update(params + "9b288147e5474dd2aa67085f716c560d").digest("hex"),
+					playUrl = `http://bangumi.bilibili.com/player/web_api/playurl?${params}&sign=${sign}`;
+				getData(playUrl, true);
+			} else {
+				if (!result[pid - 1].cid) {
+					showError("获取视频 cid 出错！");
+					return;
+				}
+				video.cid = result[pid - 1].cid;
+				let params = `appkey=iVGUTjsxvpLeuDCf&cid=${video.cid}&otype=json&qn=112&quality=112&type=`,
+					sign = crypto.createHash("md5").update(params + "aHRmhWMLkdeMuILqORnYZocwMBpMEOdt").digest("hex"),
+					playUrl = `http://interface.bilibili.com/v2/playurl?${params}&sign=${sign}`;
+				getData(playUrl);
 			}
-			getData(playUrl);
 			getDanmaku(); //获取cid后，获取下载链接和弹幕信息
 			$("#nav").show();
 			if ($(".info").eq(1).is(":hidden")) {
@@ -104,6 +120,7 @@ function getData(url, isBangumi) {
 	fetch(url)
 		.then(response => response.text())
 		.then(result => {
+			console.log("PLAY URL", result);
 			var data = isBangumi ? $(result) : JSON.parse(result),
 				target = isBangumi ? data.find("durl") : data.durl;
 			if (target) {
@@ -119,12 +136,12 @@ function getData(url, isBangumi) {
 						15: "流畅 360P"
 					} //需要修改，不是一一对应
 				$("#quality").html(qualityArray[quality] || "未知");
-				parseData(target, isBangumi);
-			} else {
-				if (isBangumi) return;
-				var params = `cid=${cid}&module=movie&player=1&quality=112&ts=1`;
-				sign = crypto.createHash("md5").update(params + "9b288147e5474dd2aa67085f716c560d").digest("hex");
-				getData(`http://bangumi.bilibili.com/player/web_api/playurl?${params}&sign=${sign}`, true);
+				$("#success").show();
+				$("#cid").html(video.cid);
+				$("tbody").eq(0).html("");
+				$("#backup-url, #error").hide();
+				links = [];
+				isBangumi ? parseDataBangumi(target, links) : parseData(target, links);
 			}
 		})
 		.catch(error => {
@@ -132,13 +149,8 @@ function getData(url, isBangumi) {
 		});
 }
 
-function parseData(target, isBangumi) {
-	if (!isBangumi) $("#backup-url, #error").hide();
-	$("#success").show();
-	$("#cid").html(cid);
-	$("tbody").eq(0).html("");
-	links = [];
-	if (isBangumi) target.each((i, o) => {
+function parseDataBangumi(target, links) {
+	target.each((i, o) => {
 		var part = $(o);
 		links.push(part.find("url").text());
 		$("tbody").eq(0).append(`<tr>
@@ -154,7 +166,10 @@ function parseData(target, isBangumi) {
 			</td>
 		</tr>`);
 	});
-	else for (let part of target) {
+}
+
+function parseData(target, links) {
+	for (let part of target) {
 		links.push(part.url);
 		$("tbody").eq(0).append(`<tr>
 			<td>${part.order}</td>
@@ -189,7 +204,7 @@ function download() {
 	var flag = true;
 	[...document.querySelectorAll('input[type="checkbox"]')].forEach((element, i) => {
 		if (!element.checked || downloadArray.includes(links[i])) return;
-		$("#download").append(`<span>${cid}-${i}</span>
+		$("#download").append(`<span>${video.cid}-${i}</span>
 			<span class="speed"></span>
 			<span class="eta"></span>
 			<span class="addon"></span>
@@ -212,7 +227,7 @@ function openPath() {
 
 function downloadLink(i) {
 	let downloadPath = $("#downloadPath").val(),
-		filename = $("#videoName").val() || videoName || cid,
+		filename = $("#videoName").val() || video.name || video.cid,
 		file = path.join(downloadPath, `${filename}-${i}.flv`);
 	fs.stat(file, (error, state) => {
 		var options = {
@@ -221,14 +236,14 @@ function downloadLink(i) {
 			headers: {
 				"Range": `bytes=${state ? state.size : 0}-`, //断点续传
 				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
-				"Referer": videoUrl
+				"Referer": video.url
 			}
 		};
 		var downloads = fs.createWriteStream(file, state ? {"flags": "a"} : {}),
 			j = downloadArray.indexOf(options.url);
 		generalDownload(j, options, downloads);
 		state && $(".addon").eq(j).html(`从${Math.round(state.size / 1048576)}MiB处恢复的下载`);
-		//console.log(cid, file, options.url);
+		//console.log(video.cid, file, options.url);
 	});
 }
 
