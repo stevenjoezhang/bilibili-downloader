81291ae03f04e320534af3dbb4a346cd0ef53462
Mimi <1119186082@qq.com>
2020-09-03 16:33:54 +0800

    Set enableRemoteModule

--- app/main.js
+++ app/main.js
@@ -19,7 +19,8 @@ function createPanel() {
 		fullscreenable: false,
 		title         : "Mimi Downloader",
 		webPreferences: {
-			nodeIntegration: true
+			nodeIntegration: true,
+			enableRemoteModule: true
 		}
 	});
 
